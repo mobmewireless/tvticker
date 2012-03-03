8654f6cdc3d4303657dbcbdb2dd5463940515cf9
Vishnu Ravi <vishnu.r@mobme.in>
2012-03-03 12:00:37 +0530

    UX improved - Added a menu option to select/deselect all channels.

--- src/in/mobme/tvticker/SetFavouriteChannelsActivity.java
+++ src/in/mobme/tvticker/SetFavouriteChannelsActivity.java
@@ -1,7 +1,7 @@
 package in.mobme.tvticker;
 
-import in.mobme.tvticker.R;
 import in.mobme.tvticker.adapter.FavouriteChannelsArrayAdapter;
+import in.mobme.tvticker.adapter.ViewPagerAdapter;
 import in.mobme.tvticker.data_model.FavouriteChannelModel;
 import in.mobme.tvticker.database.Models.ChannelsInfo;
 import in.mobme.tvticker.database.TvTickerDBAdapter;
@@ -9,35 +9,92 @@ import in.mobme.tvticker.database.TvTickerDBAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.Menu;
+import android.support.v4.view.MenuItem;
+import android.view.KeyEvent;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class SetFavouriteChannelsActivity extends Activity {
-	TvTickerDBAdapter dbAdapter = null;
-	ListView listView;
-	List<FavouriteChannelModel> list = new ArrayList<FavouriteChannelModel>();
+public class SetFavouriteChannelsActivity extends FragmentActivity {
+	private TvTickerDBAdapter dbAdapter = null;
+	private ListView listView;
+	private List<FavouriteChannelModel> list = new ArrayList<FavouriteChannelModel>();
+	private static final int SELECT_ALL_MENU_ITEM = Menu.FIRST;
+	public static int selectedCount = 0;
+	private int cursorCount = 0;
+	private boolean allChannelsSelected = false;
+	private FavouriteChannelsArrayAdapter adapter;
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, SELECT_ALL_MENU_ITEM, 0, "select_all")
+				.setIcon(getSelectAllDrawable(checkChannelCounts()))
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+		return super.onCreateOptionsMenu(menu);
+	}
 
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		dbAdapter = new TvTickerDBAdapter(getBaseContext());
-
+		dbAdapter.open();
 		setContentView(R.layout.default_listview);
+		getSupportActionBar().setTitle(
+				getResources().getString(R.string.fav_channel_page_title));
 		listView = (ListView) findViewById(android.R.id.list);
 		listView.setItemsCanFocus(false);
-		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
 		list = getModel();
-		listView.setAdapter(new FavouriteChannelsArrayAdapter(this,
-				R.layout.favourite_channels, list));
+		adapter = new FavouriteChannelsArrayAdapter(this,
+				R.layout.favourite_channels, list, dbAdapter);
+		listView.setAdapter(adapter);
+	}
 
+	private int getSelectAllDrawable(boolean isTrue) {
+		return isTrue ? R.drawable.btn_check_buttonless_on
+				: R.drawable.btn_check_buttonless_off;
+	}
+
+	private boolean checkChannelCounts() {
+		if (selectedCount == cursorCount) {
+			allChannelsSelected = true;
+			return true;
+		} else {
+			allChannelsSelected = false;
+			return false;
+		}
+	}
+
+	// Action bar menu item click listener.
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case SELECT_ALL_MENU_ITEM:
+			allChannelsSelected = !allChannelsSelected;
+			item.setIcon(getSelectAllDrawable(allChannelsSelected));
+			dbAdapter.setAllChannelsAsFavorites(allChannelsSelected);
+			selectAllInList(allChannelsSelected);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	// toggles select all option
+	private void selectAllInList(boolean isSelected) {
+		for (int i = 0; i < listView.getCount(); i++) {
+			list.get(i).setSelected(isSelected);
+		}
+		selectedCount = (!isSelected) ? 0 : cursorCount;
+		adapter.notifyDataSetChanged();
 	}
 
 	private List<FavouriteChannelModel> getModel() {
-		dbAdapter.open();
+		selectedCount = 0;
 		Cursor cursor = dbAdapter.fetchAllChannels();
+		cursorCount = cursor.getCount();
 		List<FavouriteChannelModel> list = new ArrayList<FavouriteChannelModel>();
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
@@ -47,16 +104,17 @@ public class SetFavouriteChannelsActivity extends Activity {
 					.getColumnIndex(ChannelsInfo.CHANNEL_NAME));
 			boolean isSelected = getSelected(cursor.getInt(cursor
 					.getColumnIndex(ChannelsInfo.IS_FAVORITE_CHANNEL)));
+			if (isSelected) {
+				selectedCount += 1;
+			}
 			list.add(get(id, channelName, isSelected));
 			cursor.moveToNext();
 
 		}
 		cursor.close();
-		dbAdapter.close();
+
 		return list;
 	}
-	
-	
 
 	private FavouriteChannelModel get(long id, String tag, boolean selected) {
 		return new FavouriteChannelModel(id, tag, selected);
@@ -67,12 +125,22 @@ public class SetFavouriteChannelsActivity extends Activity {
 	}
 
 	@Override
-	public void onBackPressed() {
-		Toast.makeText(this, "Saving..", Toast.LENGTH_SHORT).show();
-		dbAdapter.open();
-		dbAdapter.setFavoriteChannels(list);
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			if (selectedCount == 0) {
+				Toast.makeText(getBaseContext(), "Please select atleast one channel !", Toast.LENGTH_SHORT).show();
+				return false;
+			} 
+		}
+		Toast.makeText(getBaseContext(), "Applying changes..", Toast.LENGTH_SHORT).show();
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	protected void onDestroy() {
 		dbAdapter.close();
-		super.onBackPressed();
+		ViewPagerAdapter.staticAdapterObj.notifyDataSetChanged();
+		super.onDestroy();
 	}
 
 }
--- src/in/mobme/tvticker/adapter/FavouriteChannelsArrayAdapter.java
+++ src/in/mobme/tvticker/adapter/FavouriteChannelsArrayAdapter.java
@@ -1,12 +1,13 @@
 package in.mobme.tvticker.adapter;
 
 import in.mobme.tvticker.R;
+import in.mobme.tvticker.SetFavouriteChannelsActivity;
 import in.mobme.tvticker.data_model.FavouriteChannelModel;
+import in.mobme.tvticker.database.TvTickerDBAdapter;
 
 import java.util.List;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,24 +15,28 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
 
-public class FavouriteChannelsArrayAdapter extends ArrayAdapter<FavouriteChannelModel> {
+public class FavouriteChannelsArrayAdapter extends
+		ArrayAdapter<FavouriteChannelModel> {
 
 	Context context = null;
 	List<FavouriteChannelModel> list = null;
 	int rowViewResourceId;
-	
+	private TvTickerDBAdapter dbAdapter;
+
 	public FavouriteChannelsArrayAdapter(Context context,
-			int textViewResourceId, List<FavouriteChannelModel> list) {
+			int textViewResourceId, List<FavouriteChannelModel> list,
+			TvTickerDBAdapter dbAdapter) {
 		super(context, textViewResourceId, list);
 		this.context = context;
 		this.list = list;
 		this.rowViewResourceId = textViewResourceId;
+		this.dbAdapter = dbAdapter;
 	}
 
 	static class ViewHolder {
 		protected CheckedTextView checkedText;
 	}
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = null;
@@ -40,17 +45,21 @@ public class FavouriteChannelsArrayAdapter extends ArrayAdapter<FavouriteChannel
 			view = inflator.inflate(rowViewResourceId, null);
 			final ViewHolder viewHolder = new ViewHolder();
 
-			viewHolder.checkedText = (CheckedTextView) view.findViewById(R.id.cv_id);
-			
+			viewHolder.checkedText = (CheckedTextView) view
+					.findViewById(R.id.cv_id);
+
 			viewHolder.checkedText.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					toggle(v);
-					FavouriteChannelModel element = (FavouriteChannelModel) viewHolder.checkedText.getTag();
-					Log.i("DEbug", "cur_status" + viewHolder.checkedText.isChecked());
+					FavouriteChannelModel element = (FavouriteChannelModel) viewHolder.checkedText
+							.getTag();
 					element.setSelected(viewHolder.checkedText.isChecked());
-					Log.i("DEbug", "clicked me !");
-					
+					dbAdapter.setFavoriteChannel(element);
+					if (viewHolder.checkedText.isChecked())
+						SetFavouriteChannelsActivity.selectedCount += 1;
+					else
+						SetFavouriteChannelsActivity.selectedCount -= 1;
 				}
 			});
 			view.setTag(viewHolder);
@@ -59,17 +68,16 @@ public class FavouriteChannelsArrayAdapter extends ArrayAdapter<FavouriteChannel
 			view = convertView;
 			((ViewHolder) view.getTag()).checkedText.setTag(list.get(position));
 		}
-		
+
 		ViewHolder holder = (ViewHolder) view.getTag();
 		holder.checkedText.setText(list.get(position).getName());
 		holder.checkedText.setChecked(list.get(position).isSelected());
 		return view;
 	}
-	
+
 	public void toggle(View v) {
 		CheckedTextView cView = (CheckedTextView) v.findViewById(R.id.cv_id);
-		Log.i("DEbug", "toggle !");
 		cView.toggle();
 	}
-	
+
 }
